76335906385f297c33e6bfb0673f0e62454f75c6
edvalenzuela <edvalenzuelap@gmail.com>
2023-05-04 19:15:11 -0400

    feat: add show a public property

--- controllers/propiedadController.js
+++ controllers/propiedadController.js
@@ -266,6 +266,28 @@ const eliminar = async(req, res) => {
 
 }
 
+const mostrarPropiedad = async(req, res) => {
+
+  const { id } = req.params
+
+  //comprobar que la propiedad exista
+  const propiedad = await Propiedad.findByPk(id, {
+    include:[
+      { model: Precio, as: 'precio'},
+      { model: Categoria, as: 'categoria'}
+    ]
+  })
+
+  if(!propiedad){
+    return res.redirect('/404')
+  }
+
+  res.render('propiedades/mostrar', {
+    propiedad,
+    pagina: propiedad.titulo
+  })
+}
+
 export {
   admin,
   crear,
@@ -274,5 +296,6 @@ export {
   almacenarImagen,
   editar,
   guardarCambios,
-  eliminar
+  eliminar,
+  mostrarPropiedad
 }
